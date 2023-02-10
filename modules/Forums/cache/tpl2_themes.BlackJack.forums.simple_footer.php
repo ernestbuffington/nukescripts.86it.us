01b1af93f9cdb252d4db0dd9b0a1992a76d68cb1
Ernest Buffington <ernest.buffington@gmail.com>
2023-02-10 03:14:07 -0500

    Initial Commit 2/10/2023

---
+++ modules/Forums/cache/tpl2_themes.BlackJack.forums.simple_footer.php
@@ -0,0 +1,10 @@
+<?php
+
+// eXtreme Styles mod cache. Generated on Wed, 08 Feb 2023 18:38:23 +0000 (time=1675881503)
+
+?><div style="display:none" id="resizemod"></div>
+<div align="center"><span class="copyright"><br /><?php echo isset($this->vars['REPORT_LINK']) ? $this->vars['REPORT_LINK'] : $this->lang('REPORT_LINK'); ?><br /><br /><?php echo isset($this->vars['ADMIN_LINK']) ? $this->vars['ADMIN_LINK'] : $this->lang('ADMIN_LINK'); ?><br />
+<?php echo isset($this->vars['TRANSLATION_INFO']) ? $this->vars['TRANSLATION_INFO'] : $this->lang('TRANSLATION_INFO'); ?>
+<?php if ($this->vars['TRANSLATION_INFO']) {  ?>
+<?php } ?>
+</div>
